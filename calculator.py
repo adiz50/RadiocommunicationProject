2d06f9d93f95225f6b6b516234d52438245e334b
adiz50 <adribox12@gmail.com>
2022-04-02 16:11:16 +0200

    For Bartek

--- calculator.py
+++ calculator.py
@@ -24,6 +24,13 @@ def wall_once_reflected_path_length_and_angle(x):
 
     return path, angle
 
+def ceiling_once_reflected_path_length_and_angle(x):
+    los = los_length(x)
+    c_side = math.sqrt(height+(los/2)**2)
+    path = math.sqrt(5**2+los**2)
+    angle = math.degrees(math.asin(height*2/path))
+
+    return path, angle
 
 # x - distance from left wall to receiver
 def los_length(x):
@@ -39,6 +46,8 @@ x = np.linspace(0.0, 4.0, 200)
 for i in range(x.size):
     print('x='+str(x[i]))
     print('LOS_path ' + str(los_length(x[i])))
-    print('Reflected path ' + str(wall_once_reflected_path_length_and_angle(x[i])))
+    print('Reflected path from wall ' + str(wall_once_reflected_path_length_and_angle(x[i])))
+    print('Reflected path from ceiling ' + str(ceiling_once_reflected_path_length_and_angle(x[i])) + "\n")
+
 
 
