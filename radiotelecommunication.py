14cd02f6d8744359d8c863f7f90977cd387e136f
Rylyegh <bartlomiej.sojka@student.put.poznan.pl>
2022-04-06 12:35:09 +0200

    Daniel śmierdzi v2

--- radiotelecommunication.py
+++ radiotelecommunication.py
@@ -11,6 +11,7 @@ import scipy as sp
 import matplotlib.pyplot as plt
 import imageio
 from scipy import signal
+from matplotlib.widgets import Slider, Button
 
 # fi = -(2*pi*fc*di)/light
 
@@ -23,9 +24,9 @@ from scipy import signal
 # v = h*sqrt((2/lam)*(s1+s2/r1*r2))
 # C = 6.9+20*log(sqrt((v-0.1)^2+1)+v-0.1)
 
-#############
-# .        # #
-#  .      #   #
+  ##############
+ #  .        # #
+#   .       #  #
 ###########    #
 #   .      #   #
 #   .      #   #
@@ -39,11 +40,11 @@ length = 5
 height = 2.5
 width = 4
 
-samples = 200
+samples_init = 2000
 
 concrete = 5.31
-light = 3 * 10 ** 8
-fc = 2.4 * 10 ** 9
+light = 3e8
+fc = 2.4e9
 lam = light / fc
 
 los = []
@@ -55,7 +56,7 @@ current_angle = []
 angles_ceiling = []
 angles_wall = []
 
-x1 = np.linspace(0.0, 4.0, samples)
+x1 = np.linspace(0.0, 4.0, samples_init)
 
 def LOS_length(x):
     if (x > width / 2):
@@ -75,18 +76,18 @@ def wall_once_reflected_path_length_and_angle(x):
     c2_side = math.sqrt(a2_side**2 + (width/2)**2)
 
     path = c1_side + c2_side
-    angle = math.degrees(math.asin(x/c1_side))
+    angle = math.asin(x/c1_side)
 
     return path, angle
 
 def ceiling_once_reflected_path_length_and_angle(x):
     los = LOS_length(x)
     path = math.sqrt((height*2)**2+los**2)
-    angle = math.degrees(math.asin(height*2/path))
+    angle = math.asin(height*2/path)
 
     return path, angle
 
-for i in range(samples):
+for i in range(samples_init):
     los_left = LOS_length(x1[i])
     los.append(los_left)
     multipath_wall, multipath_wall_angle = wall_once_reflected_path_length_and_angle(x1[i])
@@ -107,10 +108,15 @@ def reflectance(angle):
                 math.cos(angle) + math.sqrt(concrete - math.sin(angle) ** 2))
     return a
 
+def diffraction():
 
-def received_power_multipath1(path_los, path_wall, path_ceiling, array):
-    i = samples
-    for x in range(i):
+    v = h*math.sqrt((2/lam)*(s1s2)/(r1*r2))
+    diff = 6.9+20*math.log10(math.sqrt((v-0.1)**2+1)+v-0.1)
+
+    return diff
+
+def received_power_multipath1(path_los, path_wall, path_ceiling, array, samples):
+    for x in range(samples):
         fi1 = -1 * (2 * pi * fc * path_los[x]) / light
         fi2 = -1 * (2 * pi * fc * path_wall[x]) / light
         fi3 = -1 * (2 * pi * fc * path_ceiling[x]) / light
@@ -118,27 +124,36 @@ def received_power_multipath1(path_los, path_wall, path_ceiling, array):
         P1 = (1 / path_los[x]) * cmath.exp(pi * fi1 * 1j)  # LOS
         P2 = (reflectance(angles_wall[x]) / path_wall[x]) * cmath.exp(pi * fi2 * 1j)  # Reflected path wall
         P3 = (reflectance(angles_ceiling[x]) / path_ceiling[x]) * cmath.exp(pi * fi3 * 1j)  # Reflected path ceiling
-        sum = abs(P1 + P2 + P3) ** 2
+        sum = 10*math.log10(abs(P1 + P2 + P3) ** 2)
         array.append(sum)
+        if(x==601):
+            print(sum)
+            print(reflectance(angles_wall[x]),angles_wall[x])
+            print(reflectance(angles_ceiling[x]),angles_ceiling[x])
+
+
 
     return array
 
 
-def received_power_los(path, array):
-    i = samples
-    for x in range(i):
+def received_power_los(path, array, samples):
+    for x in range(samples):
         fi = -1 * (2 * pi * fc * path[x]) / light
-        P = abs((1 / path[x]) * cmath.exp(pi * fi * 1j)) ** 2
+        P = 10*math.log10(abs((1 / path[x]) * cmath.exp(pi * fi * 1j)) ** 2)
         array.append(P)
     return array
 
 
-LOS_line = received_power_los(los, PrP0_los)
-multipath = received_power_multipath1(los, wall, ceiling, PrP0_multipath)
+LOS_line = received_power_los(los, PrP0_los,samples_init)
+multipath = received_power_multipath1(los, wall, ceiling, PrP0_multipath,samples_init)
 
+
+plt.xlabel('Distance from the left wall [cm]')
+plt.ylabel('Pr/P0')
 # y is distance from the transmitter
 plt.plot(LOS_line, label="LOS line")
 plt.plot(multipath, label="Multipath 1")
+
 
 plt.title('Multipath fading')
 # show a legend on the plot
